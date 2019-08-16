c5be0160d944ee37be004648a514d5629d23f98f
Michael Sheinberg <mike@freedom.press>
2019-08-15 17:34:53 -0700

    Get tar and encrypted upload logic in place
    Could use some more tests but lets get back to that later

--- gsutil_sync/bucket_sync.py
+++ gsutil_sync/bucket_sync.py
@@ -4,9 +4,11 @@
 #
 #
 import argparse
+import datetime
 import logging
 import os
 import subprocess
+import tarfile
 import tempfile
 import sys
 
@@ -29,6 +31,7 @@ class GCPBucketBackup(object):
         src_bucket: str,
         backup_bucket: str,
         encrypt_key: str,
+        file_suffix: str,
         gsutil_path="/google-cloud-sdk/bin",
     ):
 
@@ -36,18 +39,36 @@ class GCPBucketBackup(object):
         self.dst = backup_bucket
         self.encrypt_key = encrypt_key
         self.gsutil_path = gsutil_path
+        self.suffix = file_suffix
+
+    def _subprocess_debug_wrap(self, cmd: list, shellmode: bool = False) -> str:
+        logger.debug("Calling command {}".format(" ".join(cmd)))
+        if shellmode:
+            cmd = " ".join(cmd)
+        copy_output = subprocess.check_output(
+            cmd, stderr=subprocess.STDOUT, shell=shellmode).decode('ascii')
+        logger.debug(copy_output)
+
+        return copy_output
+
+    def gsutil_encrypt_cp_cmd(self, src: str, dst: str) -> None:
+        """ Copy a local file to a bucket with encryption """
+        gsutil_base_cmd = [self.gsutil_path,
+                           "-o", '"GSUtil:encryption_key={}"'.format(
+                               self.encrypt_key),
+                           "cp",  src, dst]
+
+        self._subprocess_debug_wrap(gsutil_base_cmd, True)
 
     def rsync_cmd(self, src: str, dst: str, dry: bool = False) -> None:
-        """ Thin wrapper around gsutil rsync command """
+        """ Call gsutil rsync against two paths """
         gsutil_base_cmd = [self.gsutil_path,
                            "-m", "rsync", "-r", "-d", src, dst]
 
         if dry:
             gsutil_base_cmd.insert(4, "-n")
 
-        rsync_output = subprocess.check_output(
-            gsutil_base_cmd, stderr=subprocess.STDOUT).decode('ascii')
-        logger.debug(rsync_output)
+        self._subprocess_debug_wrap(gsutil_base_cmd)
 
     def rsync_source_bucket(self) -> str:
         """ Pull down a copy of a bucket contents for local comparison"""
@@ -61,11 +82,32 @@ class GCPBucketBackup(object):
 
         return self.tmp_dir
 
+    def tar_directory(self, tar_dir: str) -> str:
+        """ Tar+gzip up a directory and return path to that tar ball """
+        tar_file_path = os.path.join(tar_dir, "src-bucket.tar.gz")
+        tar = tarfile.open(tar_file_path, 'w:gz')
+        tar.add(tar_dir, arcname='')
+        tar.close()
+        logger.info("Created tar at {} of {}".format(tar_file_path, tar_dir))
+
+        return tar_file_path
+
+    def upload_encrypted_timestamp_file(self, upload_file: str) -> None:
+        """ Given a file path upload said file to our backup bucket. File is timestamp'd and includes file prefix. """
+        timestamp_name = datetime.datetime.now().strftime(
+            '%b-%d-%y-%X-{}.tar.gz'.format(self.suffix))
+        backup_bucket_path = os.path.join(self.dst, timestamp_name)
+
+        self.gsutil_encrypt_cp_cmd(upload_file, backup_bucket_path)
+
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser(description=__doc__)
     args.add_argument('src', type=str, help="Source bucket")
     args.add_argument('backup', type=str, help="Backup bucket")
+    args.add_argument(
+        'suffix', type=str,
+        help="Backup suffix filename ex: $timestamp-prefix.tar.gz")
     args.add_argument('-v', action='store_true', default=False,
                       required=False, help='Increase verbosity')
     args.add_argument('-gsutil_bin', type=str, required=False,
@@ -86,5 +128,14 @@ if __name__ == "__main__":
         sys.exit(1)
 
     backup = GCPBucketBackup(
-        parsed_args.src, parsed_args.backup, encrypt_key, parsed_args.gsutil_bin)
-    backup.rsync_source_bucket()
+        parsed_args.src, parsed_args.backup, encrypt_key,
+        parsed_args.suffix, parsed_args.gsutil_bin)
+
+    # Local directory filled with rsync'd files
+    backup_local_dir = backup.rsync_source_bucket()
+
+    # Create tarfile and grab file location
+    local_tar = backup.tar_directory(backup_local_dir)
+
+    # Upload backup to final destination
+    backup.upload_encrypted_timestamp_file(local_tar)
--- gsutil_sync/test_bucket_sync.py
+++ gsutil_sync/test_bucket_sync.py
@@ -1,33 +1,46 @@
 import unittest.mock
 from bucket_sync import GCPBucketBackup
 
-GENERIC_ARGS = dict(src_bucket="gs://src-bucket/dir/",
-                    backup_bucket="gs://backup-bucket/dir", encrypt_key="124124213123")
 
+class TestBucketSync(object):
+    def setup_class(self):
+        self.GENERIC_ARGS = dict(src_bucket="gs://src-bucket/dir/",
+                                 backup_bucket="gs://backup-bucket/dir",
+                                 encrypt_key="124124213123",
+                                 file_suffix="siteapp")
+        self.gen_backup_obj = GCPBucketBackup(**self.GENERIC_ARGS)
 
-def test_rsync_cmd_cli_str():
-    """Throw different options against rsync function to determine called command"""
+    def test_rsync_cmd_cli_str(self):
+        """Throw different options against rsync function to determine called command"""
 
-    backup = GCPBucketBackup(**GENERIC_ARGS)
-    with unittest.mock.patch("subprocess.check_call") as mock_subp:
-        # With dry-run
-        backup.rsync_cmd(backup.src, backup.dst, dry=True)
-        mock_subp.assert_called_with(
-            [backup.gsutil_path, "-m", "rsync", "-r", "-n", "-d",
-                GENERIC_ARGS['src_bucket'], GENERIC_ARGS['backup_bucket']]
-        )
+        backup = self.gen_backup_obj
 
-        # Without dry-run
-        backup.rsync_cmd(backup.src, backup.dst)
-        mock_subp.assert_called_with(
-            [backup.gsutil_path, "-m", "rsync", "-r", "-d",
-                GENERIC_ARGS['src_bucket'], GENERIC_ARGS['backup_bucket']]
-        )
+        with unittest.mock.patch("subprocess.check_output") as mock_subp:
 
-        # Lets try swapping out the gsutil config path
-        backup.gsutil_path = '/bin/gsutil'
-        backup.rsync_cmd(backup.src, backup.dst)
-        mock_subp.assert_called_with(
-            ['/bin/gsutil', "-m", "rsync", "-r", "-d",
-                GENERIC_ARGS['src_bucket'], GENERIC_ARGS['backup_bucket']]
-        )
+            assert_call_skel = [backup.gsutil_path, "-m", "rsync", "-r", "-d",
+                                self.GENERIC_ARGS['src_bucket'], self.GENERIC_ARGS['backup_bucket']]
+            assert_call_kws = dict(stderr=-2, shell=False)
+            # With dry-run
+            backup.rsync_cmd(backup.src, backup.dst, dry=True)
+            cmd_dry = assert_call_skel[:4] + ["-n"] + assert_call_skel[4:]
+            mock_subp.assert_called_with(cmd_dry, **assert_call_kws)
+
+            # Without dry-run
+            backup.rsync_cmd(backup.src, backup.dst)
+            mock_subp.assert_called_with(assert_call_skel, **assert_call_kws)
+
+            # Lets try swapping out the gsutil config path
+            backup.gsutil_path = '/bin/gsutil'
+            cmd_binswap = [backup.gsutil_path] + assert_call_skel[1:]
+            backup.rsync_cmd(backup.src, backup.dst)
+            mock_subp.assert_called_with(cmd_binswap, **assert_call_kws)
+
+    def test_tar_invocation(self):
+        """Ensure we are calling tar with the expected path and returning a full tarfile path"""
+
+        with unittest.mock.patch("tarfile.open") as mock_tar:
+            tar_action = self.gen_backup_obj.tar_directory("/tmp/dir")
+            full_bucket_path = "/tmp/dir/src-bucket.tar.gz"
+
+            assert tar_action == full_bucket_path
+            mock_tar.assert_called_once_with(full_bucket_path, 'w:gz')
